5577949d35ef05c25b786359be516d6f1eaeb2e2
Brandon Golden <brandongolden@Brandons-MacBook-Pro.local>
2016-05-05 18:19:06 -0400

    added dictionary
    dictionary holds values that relate to my array values

--- Madlib/madlib.py
+++ Madlib/madlib.py
@@ -8,3 +8,6 @@ story = ["Donald Trump", "Jimmy Fallon"]
 for i in range(0,3):
 	story.append(raw_input("Enter a number between 1 and 10:  "))
 	i = i+1
+
+tvshows = dict()
+tvshows = {story[0]:"The Apprentice", story[1]:"The Tonight Show"}
