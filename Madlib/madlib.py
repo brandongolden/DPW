5deb5f6b6d39936cadf226571256cd05762d3f02
Brandon Golden <brandongolden@Brandons-MacBook-Pro.local>
2016-05-05 19:36:22 -0400

    print story
    story is put together using the print function

--- Madlib/madlib.py
+++ Madlib/madlib.py
@@ -20,16 +20,18 @@ if trump_wallet > fallon_wallet:
 elif fallon_wallet > trump_wallet:
 	a = "Jimmy Fallon is willing to pay more than Donald Trump for you to guest star in his TV show. "
 else:
-	a = "Both Donald Trump and Jimmy Fallon have offered to pay $" + str(trump_wallet) + " for you to guest star in their TV show."
+	a = "Both Donald Trump and Jimmy Fallon have offered to pay $" + str(trump_wallet) + " for you to guest star in their TV show. "
 
 if fav_tvshow == tvshows[story[0]]:
-	b = "Donald Trump and you have something in common."
+	b = "Donald Trump and you have something in common. "
 elif fav_tvshow == tvshows[story[1]]:
-	b = "Jimmy Fallon and you have something in common."
+	b = "Jimmy Fallon and you have something in common. "
 
 def story_sentence(y, z):
-	make_sentence = "Hi " + y + "!" + " The " + z + " socks you are wearing defiantly wont go unnoticed."
+	make_sentence = "Hi " + y + "!" + " The " + z + " socks you are wearing defiantly don't go unnoticed. "
 	return make_sentence
 
 d = story_sentence(first_name, fav_color);
 #print d
+
+print d + "Tonight Donald Trump and Jimmy Fallon want you to guest star in their TV show they host. " + a + b
