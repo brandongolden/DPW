35fba7365181fe70e78a32d11450a723c717fb00
Brandon Golden <brandongolden@Brandons-MacBook-Pro.local>
2016-05-05 18:12:45 -0400

    user input
    3 strings and 3 numbers are obtained from user using raw_input

---
+++ Madlib/madlib.py
@@ -0,0 +1,10 @@
+first_name = raw_input("Enter your name:  ")
+fav_color = raw_input("Enter your favorite color:  ")
+fav_tvshow = raw_input("Enter your favorite TV show:  ")
+
+story = ["Donald Trump", "Jimmy Fallon"]
+
+
+for i in range(0,3):
+	story.append(raw_input("Enter a number between 1 and 10:  "))
+	i = i+1
