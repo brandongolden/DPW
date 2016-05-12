3969cf42b3aee87c96d5f615f3e5ccdc502b75be
Brandon Golden <brandongolden@Brandons-MacBook-Pro.local>
2016-05-12 18:02:47 -0400

    Followed along with videos
    Will use this as template for project

--- simple-form/main.py
+++ simple-form/main.py
@@ -1,25 +1,35 @@
-#!/usr/bin/env python
-#
-# Copyright 2007 Google Inc.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-#
+'''
+Brandon Golden
+'''
 import webapp2
 
 class MainHandler(webapp2.RequestHandler):
-    def get(self):
-        self.response.write('Hello world!')
-
+        def get(self):
+            page_head = '''
+            <DOCTYPE HTML>
+            <html>
+                <head>
+                    <title>Simple Form</title>
+                </head>
+            <body>
+            '''
+            page_body = '''
+            <form method="GET" action="">
+            <label>Name: </label><input type="text" name="user" />
+            <label>Email: </label><input type="text" name="email" />
+            <input type="submit" value="Submit" />
+            </form>
+            '''
+            page_close = '''
+            </body>
+            </html>
+            '''
+            if self.request.GET:
+                user = self.request.GET['user']
+                email = self.request.GET['email']
+                self.response.write(page_head + user + ' ' + email + page_close)
+            else:
+                self.response.write(page_head + page_body + page_close)
 app = webapp2.WSGIApplication([
     ('/', MainHandler)
 ], debug=True)
