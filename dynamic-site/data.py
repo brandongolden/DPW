48c36278e15e57d70265e0715ae3c8cfa6a32885
Brandon Golden <brandongolden@Brandons-MacBook-Pro.local>
2016-05-26 11:53:43 -0400

    5 Data Objects
    Made from the WebHost class

--- dynamic-site/data.py
+++ dynamic-site/data.py
@@ -16,3 +16,55 @@ class WebHost(object):
         self.bandwidth = ""
         self.control_panel = ""
         self.domains = ""
+
+class Data(object):
+    def __init__(self):
+        self.dh = WebHost()
+        self.dh.company = "DreamHost"
+        self.dh.logo_img = ""
+        self.dh.plan = "Month-to-Month"
+        self.dh.price = "$10.95/mo"
+        self.dh.disk_space = "Unlimited"
+        self.dh.bandwidth = "Unlimited"
+        self.dh.control_panel = "DreamHost panel"
+        self.dh.domains = "Unlimited"
+
+        self.hg = WebHost()
+        self.hg.company = "HostGator"
+        self.hg.logo_img = ""
+        self.hg.plan = "Baby"
+        self.hg.price = "$11.95/mo"
+        self.hg.disk_space = "Unlimited"
+        self.hg.bandwidth = "Unlimited"
+        self.hg.control_panel = "cPanel"
+        self.hg.domains = "Unlimited"
+
+        self.s5 = WebHost()
+        self.s5.company = "Site5"
+        self.s5.logo_img = ""
+        self.s5.plan = "hostPro"
+        self.s5.price = "$10.95/mo"
+        self.s5.disk_space = "Unlimited"
+        self.s5.bandwidth = "Unlimited"
+        self.s5.control_panel = "cPanel"
+        self.s5.domains = "Unlimited"
+
+        self.gd = WebHost()
+        self.gd.company = "GoDaddy"
+        self.gd.logo_img = ""
+        self.gd.plan = "Deluxe"
+        self.gd.price = "$9.99/mo"
+        self.gd.disk_space = "Unlimited"
+        self.gd.bandwidth = "Unlimited"
+        self.gd.control_panel = "cPanel"
+        self.gd.domains = "Unlimited"
+
+        self.sg = WebHost()
+        self.sg.company = "SiteGround"
+        self.sg.logo_img = ""
+        self.sg.plan = "GrowBig"
+        self.sg.price = "$14.95/mo"
+        self.sg.disk_space = "20GB Web Space"
+        self.sg.bandwidth = "Suitable for ~ 25,000 Visits Monthly"
+        self.sg.control_panel = "cPanel"
+        self.sg.domains = "Unlimited"
