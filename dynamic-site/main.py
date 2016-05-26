67923a8dbe6423cb870c9438557fcde15d2e597c
Brandon Golden <brandongolden@Brandons-MacBook-Pro.local>
2016-05-26 16:52:20 -0400

    Comments main.py
    Added comments to code in main.py

--- dynamic-site/main.py
+++ dynamic-site/main.py
@@ -4,22 +4,26 @@ Brandon Golden
 DPW
 Dynamic Site
 '''
+
 import webapp2
-from data import *
-from page import *
+from data import *  # Import all classes from data.py
+from page import *  # Import all classes from page.py
 
 class MainHandler(webapp2.RequestHandler):
     def get(self):
 
+        # Make instances of Data, ContentPage and Page classes
         d = Data()
         c = ContentPage()
         p = Page()
 
-        if self.request.GET:
-            webhost = self.request.GET['webhost']
+        if self.request.GET:  # If there is a GET request run this code
+            webhost = self.request.GET['webhost']  # Set webhost to the value of the GET request
 
-            if webhost == "dreamhost":
-                w = d.webhosts_array[0]
+            if webhost == "dreamhost":  # If the value of the GET request is dreamhost run this code
+                w = d.webhosts_array[0]  # Select dreamhost from the webhosts_array
+
+                # Retrieve attributes that match webhost chosen in GET request
                 c.company = w.company
                 c.logo_img = w.logo_img
                 c.plan = w.plan
@@ -29,8 +33,10 @@ class MainHandler(webapp2.RequestHandler):
                 c.control_panel = w.control_panel
                 c.domains = w.domains
 
-            elif webhost == "hostgator":
-                w = d.webhosts_array[1]
+            elif webhost == "hostgator":  # If the value of the GET request is hostgator run this code
+                w = d.webhosts_array[1]  # Select hostgator from the webhosts_array
+
+                # Retrieve attributes that match webhost chosen in GET request
                 c.company = w.company
                 c.logo_img = w.logo_img
                 c.plan = w.plan
@@ -40,8 +46,10 @@ class MainHandler(webapp2.RequestHandler):
                 c.control_panel = w.control_panel
                 c.domains = w.domains
 
-            elif webhost == "site5":
-                w = d.webhosts_array[2]
+            elif webhost == "site5":  # If the value of the GET request is site5 run this code
+                w = d.webhosts_array[2]  # Select site5 from the webhosts_array
+
+                # Retrieve attributes that match webhost chosen in GET request
                 c.company = w.company
                 c.logo_img = w.logo_img
                 c.plan = w.plan
@@ -51,8 +59,10 @@ class MainHandler(webapp2.RequestHandler):
                 c.control_panel = w.control_panel
                 c.domains = w.domains
 
-            elif webhost == "godaddy":
-                w = d.webhosts_array[3]
+            elif webhost == "godaddy":  # If the value of the GET request is godaddy run this code
+                w = d.webhosts_array[3]  # Select godaddy from the webhosts_array
+
+                # Retrieve attributes that match webhost chosen in GET request
                 c.company = w.company
                 c.logo_img = w.logo_img
                 c.plan = w.plan
@@ -62,8 +72,10 @@ class MainHandler(webapp2.RequestHandler):
                 c.control_panel = w.control_panel
                 c.domains = w.domains
 
-            elif webhost == "siteground":
-                w = d.webhosts_array[4]
+            elif webhost == "siteground":  # If the value of the GET request is siteground run this code
+                w = d.webhosts_array[4]  # Select siteground from the webhosts_array
+
+                # Retrieve attributes that match webhost chosen in GET request
                 c.company = w.company
                 c.logo_img = w.logo_img
                 c.plan = w.plan
@@ -74,7 +86,7 @@ class MainHandler(webapp2.RequestHandler):
                 c.domains = w.domains
 
 
-            # Show custom page using info above
+            # Show custom page using selected info above
             self.response.write(c.make_page())
 
         else:
