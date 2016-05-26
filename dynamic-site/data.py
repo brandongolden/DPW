6a2e269b5f8c858a8927db0ff1f0fea7cfecb014
Brandon Golden <brandongolden@Brandons-MacBook-Pro.local>
2016-05-26 08:56:16 -0400

    DataObject
    DataObject WebHost has been made

--- dynamic-site/data.py
+++ dynamic-site/data.py
@@ -4,3 +4,15 @@ Brandon Golden
 DPW
 Dynamic Site
 '''
+
+
+class WebHost(object):
+    def __init__(self):
+        self.company = ""
+        self.logo_img = ""
+        self.plan = ""
+        self.price = ""
+        self.disk_space = ""
+        self.bandwidth = ""
+        self.control_panel = ""
+        self.domains = ""
