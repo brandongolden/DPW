6013d459fbf3f17df1258283295df9f5e9406dde
Brandon Golden <brandongolden@Brandons-MacBook-Pro.local>
2016-05-26 21:00:40 -0400

    Icons
    Icons in img folder

--- dynamic-site/page.py
+++ dynamic-site/page.py
@@ -21,15 +21,39 @@ class Page(object):
 
         # html body that will show when GET request does not exist
         self._body = '''
-        <a href="?webhost=dreamhost">DreamHost</a>
-        <a href="?webhost=hostgator">HostGator</a>
-        <a href="?webhost=site5">Site5</a>
-        <a href="?webhost=godaddy">GoDaddy</a>
-        <a href="?webhost=siteground">SiteGround</a>
+        <section>
+            <h1>Web Hosting</h1>
+            <article>
+                <a href="?webhost=dreamhost"><h2>DreamHost</h2></a>
+                <p>DreamHost is a Los Angeles-based web hosting provider and domain name registrar. It is the web hosting and cloud computing business owned by New Dream Network, LLC, founded in 1996 by Dallas Bethune, Josh Jones, Michael Rodriguez and Sage Weil, undergraduate students at Harvey Mudd College in Claremont, California, and registered in 1997 by Michael Rodriguez.[2][3] DreamHost began hosting customers' sites in 1997.[4] In May 2012, DreamHost spun off Inktank.[5] Inktank is a professional services and support company for the open source Ceph file system.[6] In November 2014, DreamHost spun off Akanda, an open source network virtualization solution.[7]</p>
+                <a href="https://en.wikipedia.org/wiki/DreamHost">https://en.wikipedia.org/wiki/DreamHost</a>
+            </article>
+            <article>
+                <a href="?webhost=hostgator"><h2>HostGator</h2></a>
+                <p>HostGator is a Houston-based provider of shared, reseller, virtual private server, and dedicated web hosting with an additional presence in Austin, Texas.[3][4]</p>
+                <a href="https://en.wikipedia.org/wiki/HostGator">https://en.wikipedia.org/wiki/HostGator</a>
+            </article>
+            <article>
+                <a href="?webhost=site5"><h2>Site5</h2></a>
+            </article>
+            <article>
+                <a href="?webhost=godaddy"><h2>GoDaddy</h2></a>
+                <p>GoDaddy is a publicly traded Internet domain registrar and web hosting company.[4] As of January 2016, GoDaddy was said to have had more than 61 million domain names under management, making it the world's largest ICANN-accredited registrar. It serves more than 13 million customers and employs more than 4,000 people.[5] The company is known for its celebrity spokespeople, Super Bowl ads and as being an online provider for small businesses. In addition to a postseason college football bowl game, it previously sponsored NASCAR. It has been involved in several controversies related to security and privacy. In addition to domain registration and hosting, GoDaddy also sells e-business related software and services.</p>
+                <a href="https://en.wikipedia.org/wiki/GoDaddy">https://en.wikipedia.org/wiki/GoDaddy</a>
+            </article>
+            <article>
+                <a href="?webhost=siteground"><h2>SiteGround</h2></a>
+                <p>SiteGround is a web hosting company founded in 2004 and servicing more than 450,000 domains world-wide. It provides shared hosting, cloud hosting and dedicated servers. Currently, the company employs over 315 people.[2]</p>
+                <a href="https://en.wikipedia.org/wiki/SiteGround">https://en.wikipedia.org/wiki/SiteGround</a>
+            </article>
+        </section>
         '''
 
         # footer that will be used on every page
         self._footer = '''
+        <footer>
+            <a href="http://www.fatcow.com/free-icons">Free Icons</a> | Brandon Golden
+        </footer>
         </body>
         </html>
         '''
@@ -56,13 +80,26 @@ class ContentPage(Page):
         # html body that will show when a GET request exists
         # local variables match chosen webhost
         self._webhost_body = '''
-        {self.company}
-        {self.plan}
-        {self.price}
-        {self.disk_space}
-        {self.bandwidth}
-        {self.control_panel}
-        {self.domains}
+        <section>
+        <h1>{self.company}</h1>
+        <img src="img/package.png">
+        <b>Plan:</b> {self.plan}<br>
+
+        <img src="img/total_plan_cost.png">
+        <b>Price:</b> {self.price}<br>
+
+        <img src="img/disk_space.png">
+        <b>Disk Space:</b> {self.disk_space}<br>
+
+        <img src="img/limit_bandwidth_usage.png">
+        <b>Bandwidth:</b> {self.bandwidth}<br>
+
+        <img src="img/control_panel_access.png">
+        <b>Control Panel:</b> {self.control_panel}<br>
+
+        <img src="img/domain_names_advanced.png">
+        <b>Domains:</b> {self.domains}
+        </section>
         '''
 
     # merge header, webhost_body and footer to make webpage for webhost
